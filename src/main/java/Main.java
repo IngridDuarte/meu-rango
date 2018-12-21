9dc59d6f1bf69972de0b70624584bc0dea571705
cassia11 <cassiagomes0102@gmail.com>
2018-12-21 14:23:35 -0200

    Ola mundo
    Criamos um programa de Ola Mundo em Java

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+  public static void main(String [] args) {
+
+    Mensagem oi = new Mensagem("Oi");
+
+    System.out.println(oi.conteudo);
+  }
+
+}
---
+++ src/main/java/Mensagem.java
@@ -0,0 +1,8 @@
+public class Mensagem {
+
+    public final String conteudo;
+
+    public Mensagem(String conteudo) {
+        this.conteudo = conteudo;
+    }
+}
